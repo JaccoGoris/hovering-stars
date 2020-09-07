c28081a40478271ed1929724d82df8dac5c96e2e
Jacco Goris <contact@jaccogoris.com>
2020-09-07 17:40:43 +0200

    moved dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // Pick one here
-const template = 'public/index.html'
+const template = "public/index.html";
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'build/stars.js',
+    path: path.resolve(__dirname, "docs/dist"),
+    filename: "build/stars.js",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
           },
         },
       },
       {
         test: /\.txt$/i,
-        use: 'raw-loader',
+        use: "raw-loader",
       },
       {
         test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: 'base64-inline-loader?limit=1000&name=[name].[ext]',
+        use: "base64-inline-loader?limit=1000&name=[name].[ext]",
       },
     ],
   },
@@ -39,4 +39,4 @@ module.exports = {
       template: path.join(__dirname, template),
     }),
   ],
-}
+};
